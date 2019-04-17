616f19b7eec6d7511be27b77f07b2dd8f734fa55
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-04-17 17:51:02 -0300

    Cambios en diseño

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-const URL_SERVER = 'http://localhost:80/workspace/comanda/public';
+const URL_SERVER = 'http://localhost/public';
 
 const ESTADO_PEDIDOS = ['CLIENTE ESPERANDO', 'CLIENTE COMIENDO', 'CLIENTE PAGANDO', 'CERRADA'];
 
