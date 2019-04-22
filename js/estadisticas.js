ccf9791b133867b36ea77115666c27b14fefe941
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-04-22 17:22:25 -0300

    Sistema de estadísticas.

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -6,45 +6,197 @@
     // Elementos.
     var $btnFiltrar;
     var $selectTipo;
+    var $selectOperacion;
     var $fechaDesde;
     var $fechaHasta;
 
-    // Modales.
-    var $modalEmpleados;
-    var $modalPedidos;
-    var $modalMesas;
-
     self.Inicializar = function () {
         // Inicialización.
-        // $modalEmpleados = $("#modalEmpleados");
-        // $modalPedidos = $("#modalPedidos");
-        // $modalMesas = $("#modalMesas");
-        // $selectTipo = $("#selectTipo");
+        $btnFiltrar = $("#btnFiltrar");
+        $selectTipo = $("#selectTipo");
+        $selectOperacion = $("#selectOperacion");
+        $fechaDesde = $("#fechaDesde");
+        $fechaHasta = $("#fechaHasta");
 
         // // Eventos.
-        // $btnFiltrar.on('click', btnFlitrar_Click)
-
-        // // Llamadas.
-        // $modalEmpleados.hide();
-        // $modalPedidos.hide();
-        // $modalMesas.hide();
+        $btnFiltrar.on('click', btnFlitrar_Click);
+        $selectTipo.on('change', selectTipo_Changed);
+        $selectOperacion.on('change', selectOperacion_Changed);
     };
 
-    function btnFlitrar_Click() {
-        // Tabla actual.
-        let $tablaModalActual;
-        let $tdTablaModalActual;
+    function selectOperacion_Changed() {
+        var $this = $(this);
+        let valorActual = $this.val();
 
-        // Realizar busqueda.
+        switch (valorActual) {
 
-        // Switch según tipo cargando el respectivo modal y mostrandolo.
-        // Es necesario hacer un find dentro del modal buscando las clases:
-        //
-        //
-    }
+            // Pedidos
+            case 'Mas vendido':
+                traerPedidoMasVendido();
+                break;
+            case 'Menos vendido':
+                traerPedidoMenosVendido();
+                break;
+            case 'Tiempo demorado':
+                traerPedidosPorTiempoDemorado();
+                break;
+            case 'Cancelados':
+                traerPedidosCancelados();
+                break;
 
-}(window.Comanda.Estadisticas = window.Comanda.Estadisticas || {}, jQuery));
+            //Mesas
+            case 'Mesa Mas usada':
+                traerMesaMasUsada();
+                break;
+            case 'Mesa Menos usada':
+                traerMenosUsada();
+                break;
+            case 'Mesa Mayor facturacion':
+                traerMesaMayorFacturacion();
+                break;
+            case 'Mesa Menor facturacion':
+                traerMesaMenorFacturacion();
+                break;
+            case 'Factura mayor importe':
+                traerMesaMayorImporte();
+                break;
+            case 'Factura menor importe':
+                traerMesaMenorImporte();
+                break;
 
-jQuery(function () {
-    window.Comanda.Estadisticas.Inicializar();
-});
+            // Empleados
+            case 'Dias y horarios de logueo':
+                traerLogeos();
+                break;
+            case 'Operaciones de todos por sector':
+                traerOperacionesDeTodosPorSector();
+                break;
+            case 'Operaciones de todos por sector p/e':
+                traerOperacionesDeTodosPorSectorPorEmpleado();
+                break;
+            case 'Cantidad de operaciones':
+                traerCantidadOperaciones();
+                break;
+        }
+
+        function traerLogeos() {
+
+        }
+
+        function traerOperacionesDeTodosPorSector() {
+
+        }
+
+        function traerOperacionesDeTodosPorSectorPorEmpleado() {
+
+        }
+
+        function traerCantidadOperaciones() {
+
+        }
+
+        function traerMesaMasUsada() {
+
+        }
+
+        function traerMenosUsada() {
+
+        }
+
+        function traerMesaMenorFacturacion() {
+
+        }
+
+        function traerMesaMayorFacturacion() {
+
+        }
+
+        function traerMesaMayorImporte() {
+
+        }
+
+        function traerMesaMenorImporte() {
+
+        }
+
+        function traerPedidoMasVendido() {
+
+        }
+
+        function traerPedidoMenosVendido() {
+
+        }
+
+        function traerPedidosPorTiempoDemorado() {
+
+        }
+
+        function traerPedidosCancelados() {
+
+        }
+
+        function selectTipo_Changed() {
+            var $this = $(this);
+            let valorActual = $this.val();
+            let html;
+
+            switch (valorActual) {
+                case 'PEDIDOS':
+                    // Segundo select opciones pedido.
+                    html += `
+                    <option>Mas vendido</option>
+                    <option>Menos vendido</option>
+                    <option>Tiempo demorado</option>
+                    <option>Cancelados</option>
+                    `;
+                    break;
+                case 'MESAS':
+                    // Segundo select opciones mesas.
+                    html += `
+                    <option>Mesa Mas usada</option>
+                    <option>Mesa Menos usada</option>
+                    <option>Mesa Mayor facturacion</option>
+                    <option>Mesa Menor facturacion</option>
+                    <option>Factura mayor importe</option>
+                    <option>Factura menor importe</option>
+                    `;
+                    break;
+                case 'EMPLEADOS':
+                    // Segundo select opciones empleados.
+                    html += `
+                    <option value="/users/log_login">Dias y horarios de logueo</option>
+                    <option>Operaciones de todos por sector</option>
+                    <option>Operaciones de todos por sector p/e</option>
+                    <option>Cantidad de operaciones</option>
+                    `;
+                    break;
+            }
+
+            $selectOperacion.html(html);
+        }
+
+        function btnFlitrar_Click() {
+            // Tabla actual.
+            let $tablaModalActual;
+            let $tdTablaModalActual;
+            let html = ``;
+
+            // Realizar busqueda.
+
+            // Switch según tipo cargando el respectivo modal y mostrandolo.
+            // Es necesario hacer un find dentro del modal buscando las clases:
+            //
+            //
+        }
+
+
+        //         <option value="/users/log_login">Dias y horarios de logueo</option>
+        //         <option>Operaciones de todos por sector</option>
+        //         <option>Operaciones de todos por sector p/e</option>
+        //         <option>Cantidad de operaciones</option>
+
+    } (window.Comanda.Estadisticas = window.Comanda.Estadisticas || {}, jQuery));
+
+    jQuery(function () {
+        window.Comanda.Estadisticas.Inicializar();
+    });
