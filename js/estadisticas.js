0d05480c0abd0799447378275e8694f861a2e9bd
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-04-22 18:00:58 -0300

    Scripting.

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -19,10 +19,13 @@
         $fechaHasta = $("#fechaHasta");
 
         // // Eventos.
-        $btnFiltrar.on('click', btnFlitrar_Click);
         $selectTipo.on('change', selectTipo_Changed);
         $selectOperacion.on('change', selectOperacion_Changed);
     };
+
+    function setearContenido(html) {
+        $('#contenido').html(html);
+    }
 
     function selectOperacion_Changed() {
         var $this = $(this);
@@ -78,81 +81,96 @@
                 traerCantidadOperaciones();
                 break;
         }
+    }
 
-        function traerLogeos() {
 
-        }
+    function traerLogeos() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerOperacionesDeTodosPorSector() {
+    function traerOperacionesDeTodosPorSector() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerOperacionesDeTodosPorSectorPorEmpleado() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerOperacionesDeTodosPorSectorPorEmpleado() {
+    function traerCantidadOperaciones() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerMesaMasUsada() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerCantidadOperaciones() {
+    function traerMenosUsada() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerMesaMenorFacturacion() {
+        setearContenido("NotImplemented");
+    }
 
-        function traerMesaMasUsada() {
+    function traerMesaMayorFacturacion() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerMesaMayorImporte() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerMenosUsada() {
+    function traerMesaMenorImporte() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerPedidoMasVendido() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerMesaMenorFacturacion() {
+    function traerPedidoMenosVendido() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function traerPedidosPorTiempoDemorado() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        function traerMesaMayorFacturacion() {
+    function traerPedidosCancelados() {
+        if (fechasValidas())
+            setearContenido("NotImplemented");
+    }
 
-        }
+    function selectTipo_Changed() {
+        var $this = $(this);
+        let valorActual = $this.val();
+        let html;
 
-        function traerMesaMayorImporte() {
-
-        }
-
-        function traerMesaMenorImporte() {
-
-        }
-
-        function traerPedidoMasVendido() {
-
-        }
-
-        function traerPedidoMenosVendido() {
-
-        }
-
-        function traerPedidosPorTiempoDemorado() {
-
-        }
-
-        function traerPedidosCancelados() {
-
-        }
-
-        function selectTipo_Changed() {
-            var $this = $(this);
-            let valorActual = $this.val();
-            let html;
-
-            switch (valorActual) {
-                case 'PEDIDOS':
-                    // Segundo select opciones pedido.
-                    html += `
+        switch (valorActual) {
+            case 'PEDIDOS':
+                // Segundo select opciones pedido.
+                html += `
                     <option>Mas vendido</option>
                     <option>Menos vendido</option>
                     <option>Tiempo demorado</option>
                     <option>Cancelados</option>
                     `;
-                    break;
-                case 'MESAS':
-                    // Segundo select opciones mesas.
-                    html += `
+                break;
+            case 'MESAS':
+                // Segundo select opciones mesas.
+                html += `
                     <option>Mesa Mas usada</option>
                     <option>Mesa Menos usada</option>
                     <option>Mesa Mayor facturacion</option>
@@ -160,43 +178,28 @@
                     <option>Factura mayor importe</option>
                     <option>Factura menor importe</option>
                     `;
-                    break;
-                case 'EMPLEADOS':
-                    // Segundo select opciones empleados.
-                    html += `
+                break;
+            case 'EMPLEADOS':
+                // Segundo select opciones empleados.
+                html += `
                     <option value="/users/log_login">Dias y horarios de logueo</option>
                     <option>Operaciones de todos por sector</option>
                     <option>Operaciones de todos por sector p/e</option>
                     <option>Cantidad de operaciones</option>
                     `;
-                    break;
-            }
-
-            $selectOperacion.html(html);
+                break;
         }
 
-        function btnFlitrar_Click() {
-            // Tabla actual.
-            let $tablaModalActual;
-            let $tdTablaModalActual;
-            let html = ``;
+        $selectOperacion.html(html);
 
-            // Realizar busqueda.
+    }
 
-            // Switch según tipo cargando el respectivo modal y mostrandolo.
-            // Es necesario hacer un find dentro del modal buscando las clases:
-            //
-            //
-        }
+    function fechasValidas() {
+        return true;
+    }
 
+}(window.Comanda.Estadisticas = window.Comanda.Estadisticas || {}, jQuery));
 
-        //         <option value="/users/log_login">Dias y horarios de logueo</option>
-        //         <option>Operaciones de todos por sector</option>
-        //         <option>Operaciones de todos por sector p/e</option>
-        //         <option>Cantidad de operaciones</option>
-
-    } (window.Comanda.Estadisticas = window.Comanda.Estadisticas || {}, jQuery));
-
-    jQuery(function () {
-        window.Comanda.Estadisticas.Inicializar();
-    });
+jQuery(function () {
+    window.Comanda.Estadisticas.Inicializar();
+});
